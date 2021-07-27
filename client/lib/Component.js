5b01e92bfc722a660640cc4255712c9fe5441dae
sshrik <chj878194@naver.com>
2021-07-28 01:01:33 +0900

    feat/add default methods.

--- client/lib/Component.js
+++ client/lib/Component.js
@@ -1,13 +1,22 @@
 export default class Component extends HTMLElement {
-  constructor() {}
+  constructor(props) {
+    super(props);
+    this.parent = props.parent;
+  }
 
-  render() {} // 부모에 붙여주게 되는 로직?
+  connectedCallback() {
+    // DOM에 Component 추가, 기본 제공 함수
+    console.log('Attached!', this);
+  }
 
-  update() {
-    if (this.shouldComponentUpdate()) {
-      this.render();
-      this.componentDidUpdate();
-    }
+  disconnectedCallback() {
+    // DOM에서 Component 제거, 기본 제공 함수
+    console.log('Detached!', this);
+  }
+
+  attributeChangedCallback(attrName, oldVal, newVal) {
+    // Component의 속성이 변경, 기본 제공 함수
+    console.log('Changed!', this, attrName, oldVal, newVal);
   }
 
   setComponentState() {}
@@ -18,9 +27,22 @@ export default class Component extends HTMLElement {
 
   getTemplate() {} // 언젠가 어디선가 쓸 수도 있는 함수
 
+  render() {} // 부모에 붙여주게 되는 로직?
+
+  update() {
+    if (this.shouldComponentUpdate()) {
+      this.render();
+      this.componentDidUpdate();
+    }
+  }
+
   componentDidUpdate() {}
 
   componentDidMount() {}
 
   shouldComponentUpdate() {}
 }
+
+// 위의 Component를 상속받은 Class를 사용하려면 다음의 등록과정이 반드시 필요합니다.
+// customElements.define('component-com', Component); // "-" 가 반드시 포함된 이름이어야 합니다.
+// 그러나 2번 실행하면 오류가 생기므로, 반드시 모든 파일에서 1회만 수행되어야 합니다.
