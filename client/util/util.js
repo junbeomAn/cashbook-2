7c06d736c9e661a00f5798e75a32faed136ee750
sshrik <chj878194@naver.com>
2021-07-28 01:08:39 +0900

    feat/util 함수 개발.

---
+++ client/util/util.js
@@ -0,0 +1,19 @@
+function deepCopy(obj) {
+  const clone = {};
+  obj.keys().forEach((key) => {
+    if (typeof obj[key] === 'object') {
+      clone[key] = deepCopy(obj[key]);
+    } else {
+      clone[key] = obj[key];
+    }
+  });
+  return clone;
+}
+
+const $ = {
+  qs: (query) => document.querySelector(query),
+  qsa: (query) => document.querySelectorAll(query),
+  create: (element) => document.createElement(element),
+};
+
+export { deepCopy, $ };
