59efb7bd496dffd3514b1607b31b0a234c369765
sshrik <chj878194@naver.com>
2021-07-29 00:09:46 +0900

    feat/Header Mockup 완성.

--- client/components/Button/Button.js
+++ client/components/Button/Button.js
@@ -6,6 +6,7 @@ export default class Header extends Component {
     super({
       ...params,
       componentName: 'button',
+      containerClass: 'smallFit',
       componentState: {
         text: params.text || '',
         textSize: params.textSize || 'small',
@@ -24,7 +25,7 @@ export default class Header extends Component {
     return `
       <button
         class="button-default button-background-${backgroundColor}" 
-        style="background-image: ${imgSrc}">
+        style="background-image: url(${imgSrc})">
           <p class="button-text-${textColor} button-text-${textSize}">${text}</p>
       </button>
     `;
--- client/components/Header/Header.js
+++ client/components/Header/Header.js
@@ -1,4 +1,7 @@
 import Component from '@/lib/Component';
+import leftArrow from '@/asset/left-arrow.svg';
+import rightArrow from '@/asset/right-arrow.svg';
+import Button from '@/components/Button/Button';
 import './Header.scss';
 
 export default class Header extends Component {
@@ -10,9 +13,65 @@ export default class Header extends Component {
     });
   }
 
-  preTemplate() {}
+  preTemplate() {
+    new Button({
+      parent: this,
+      keyword: 'history',
+      text: '내역',
+      textSize: 'small',
+      textColor: 'mint',
+      backgroundColor: 'white',
+    });
+    new Button({
+      parent: this,
+      keyword: 'calendar',
+      text: '달력',
+      textSize: 'small',
+      textColor: 'mint',
+      backgroundColor: 'white',
+    });
+    new Button({
+      parent: this,
+      keyword: 'chart',
+      text: '통계',
+      textSize: 'small',
+      textColor: 'mint',
+      backgroundColor: 'white',
+    });
+    new Button({
+      parent: this,
+      keyword: 'left-arrow',
+      imgSrc: leftArrow,
+    });
+    new Button({
+      parent: this,
+      keyword: 'right-arrow',
+      imgSrc: rightArrow,
+    });
+  }
 
   defineTemplate() {
-    return '';
+    return `
+    <div class="header-container">
+      <div class="header-contents-container">
+        <div class="logo-container">
+          <p class="logo-text">우아한 가계부</p>
+        </div>
+        <div class="header-total-date-container">
+          ${this.resolveChild('left-arrow')}
+          <div class="header-date-container">
+            <p class="header-date-month">${this.componentState.month}월</p>
+            <p class="header-date-year">${this.componentState.year}</p>
+          </div>
+          ${this.resolveChild('right-arrow')}
+        </div>
+        <div class="header-nav-container">
+          ${this.resolveChild('history')}
+          ${this.resolveChild('calendar')}
+          ${this.resolveChild('chart')}
+        </div>
+      </div>
+    </div>
+    `;
   }
 }
