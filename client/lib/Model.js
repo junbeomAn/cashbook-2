c923af0177440ce2d6b7c9d2189833dbfb77ec48
sshrik <chj878194@naver.com>
2021-08-02 18:26:19 +0900

    feature/다중 state 등록시 거절.

--- client/lib/Model.js
+++ client/lib/Model.js
@@ -19,6 +19,8 @@ export default class Model {
   initData(key, state) {
     // 초기 State 등록하는 함수.
     // key : [String] 각 페이지를 구분하는 값.
-    this._data[key] = { ...state, ...this._data[key] };
+    if (!this._data[key]) {
+      this._data[key] = { ...state, ...this._data[key] };
+    }
   }
 }
