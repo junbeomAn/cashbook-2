0c43d556cf7f0edd8224efe214337f1d9d9c4a37
junbeoman <wnsqja8744@gmail.com>
2021-08-03 22:17:10 +0900

    fix/server sequelize 쿼리 오작동 수정 및 sequelize로 안되던 부분 raw query로 일부 변경

--- server/routes/history.js
+++ server/routes/history.js
@@ -1,10 +1,19 @@
 const router = require('express').Router();
-const { sequelize, Sequelize } = require('../db/models');
-const { HISTORIES_FETCH_SUCCESS, HISTORIES_POST_SUCCESS, HISTORIES_BY_CATEGORY_FETCH_SUCCESS } = require('../utils/constant');
-const { arrangeByDate, getDateRange, formatDate, encodeHTML } = require('../utils/routes');
-
-const { History, Payment } = sequelize.models;
-// history post 요청 처리
+const { Sequelize } = require('../db/models');
+const {
+  getHistoriesOfOneMonth,
+  getHistoriesOfSixMonth,
+  createHistory,
+  updateHistory,
+} = require('../services/historyService');
+const { getPaymentId } = require('../services/paymentService');
+const {
+  HISTORIES_FETCH_SUCCESS,
+  HISTORIES_POST_SUCCESS,
+  HISTORIES_BY_CATEGORY_FETCH_SUCCESS,
+  HISTORIES_PUT_SUCCESS,
+} = require('../utils/constant');
+const { getDateRange, formatDate, encodeHTML } = require('../utils/routes');
 
 /**
  * [
@@ -12,102 +21,77 @@ const { History, Payment } = sequelize.models;
  *    date: 날짜,
  *    history: [{ ...내역 }, {...내역 }, { ...내역 }],
  *    income: 00000,
- *    expenditure: -00000 
+ *    expenditure: -00000
  *  }
  *  {
  *    date: 날짜,
  *    history: [{ ...내역 }, {...내역 }, { ...내역 }],
  *    income: 00000,
- *    expenditure: -00000 
+ *    expenditure: -00000
  *  }
  * ]
  */
 
-
-const selectPaymentMethod = sequelize.literal(`(SELECT method FROM payments WHERE payments.id = histories.PaymentId)`);
-
-
 router.get('/', async (req, res) => {
   const { userId, year, month } = req.query;
-  
   const [startDate, endDate] = getDateRange(year, month, 1);
+  const whereOptions = {
+    UserId: userId,
+    date: {
+      [Sequelize.Op.between]: [startDate, endDate],
+    },
+  };
 
-  try {
-    const histories = await History.findAll({
-      attributes: ['id', 'date', 'contents', 'amount', 'UserId', 'categoryId', [selectPaymentMethod, 'payment']],
-      where: {
-        UserId: userId,
-        date: {
-          [Sequelize.Op.between]: [startDate, endDate]
-        }
-      },
-      order: [['date', 'DESC']]
-    })
-    const result = arrangeByDate(histories);
-
+  const { result, err } = await getHistoriesOfOneMonth(whereOptions);
+  if (!err) {
     res.send({ ok: true, result, message: HISTORIES_FETCH_SUCCESS });
-  } catch (err) {
-    console.error(err);
+  } else {
+    res.send({ ok: false, err });
   }
-})
+});
 
-// 카테고리 id 별 조회 
-
-router.get('/category/:categoryId', async (req, res) => { // 중복 제거 필요한 상황
+// 카테고리 id 별 조회
+router.get('/category/:categoryId', async (req, res) => {
   const { categoryId } = req.params;
   const { userId, year, month } = req.query;
-  
+
   const [startDate, endDate] = getDateRange(year, month, 1);
+  const whereOptions = {
+    UserId: userId,
+    CategoryId: categoryId,
+    date: {
+      [Sequelize.Op.between]: [startDate, endDate],
+    },
+  };
 
-  try {
-    const histories = await History.findAll({
-      attributes: ['id', 'date', 'contents', 'amount', 'UserId', 'categoryId', [selectPaymentMethod, 'payment']],
-      where: {
-        UserId: userId,
-        CategoryId: categoryId,
-        date: {
-          [Sequelize.Op.between]: [startDate, endDate]
-        }
-      },
-      order: [['date', 'DESC']]
-    })
-    const result = arrangeByDate(histories);
-
+  const { result, err } = await getHistoriesOfOneMonth(whereOptions);
+  if (!err) {
     res.send({ ok: true, result, message: HISTORIES_FETCH_SUCCESS });
-  } catch (err) {
-    console.error(err);
+  } else {
+    res.send({ ok: false, err });
   }
-})
+});
 
 // 그래프를 구성할 해당 카테고리 6개월치 기록
 router.get('/graph/:categoryId', async (req, res) => {
   const { categoryId } = req.params;
   const { userId, year, month } = req.query;
-  const [startDate, endDate] = getDateRange(year, month, 6);
+  const date = getDateRange(year, month, 6);
+  const whereOptions = {
+    UserId: userId,
+    CategoryId: categoryId,
+    date,
+  };
 
-  try {
-    const result = await History.findAll({
-      attributes: [
-        'CategoryId',
-        [Sequelize.fn('SUM', Sequelize.col('amount')), 'total'],
-        [Sequelize.fn('MONTH', Sequelize.col('date')), 'month']
-      ],
-      where: {
-        UserId: userId,
-        CategoryId: categoryId,
-        amount: {
-          [Sequelize.Op.lt]: 0
-        },
-        date: {
-          [Sequelize.Op.between]: [startDate, endDate]
-        }
-      },
-      group: ['month'],
-      order: [['month', 'DESC']]
+  const { result, err } = await getHistoriesOfSixMonth(whereOptions);
+  if (!err) {
+    res.send({
+      ok: true,
+      result,
+      message: HISTORIES_BY_CATEGORY_FETCH_SUCCESS(categoryId),
     });
-    res.send({ ok: true, result, message: HISTORIES_BY_CATEGORY_FETCH_SUCCESS(categoryId) });
-  } catch (err) {
-    console.error(err);
+  } else {
+    res.send({ ok: false, err });
   }
 });
 
@@ -122,63 +106,69 @@ router.get('/graph/:categoryId', async (req, res) => {
  */
 
 router.post('/', async (req, res) => {
-  const { date, categoryId, contents, payment, amount } = req.body;
+  const { date, categoryId, contents, payment, amount, userId } = req.body;
   const formattedDate = formatDate(date);
-  const userId = 1;
 
-  try {
-    const PaymentId = await Payment.find({
-      where: {
-        UserId: userId,
-        method: payment
-      }
-    });
-
-    await History.create({
-      date: formattedDate,
-      contents: encodeHTML(contents),
-      amount,
-      CategoryId: categoryId,
-      PaymentId,
-      UserId: userId
-  
-    })
-    res.send({ ok: true, message: HISTORIES_POST_SUCCESS });
-  } catch(err) {
-    console.error(err);
+  const whereOptions = {
+    UserId: userId,
+    method: payment,
+  };
+  const { result: PaymentId, err } = await getPaymentId(whereOptions);
+  if (err) {
+    res.send({ ok: false, err });
+    return;
   }
-})
+  const data = {
+    date: formattedDate,
+    contents: encodeHTML(contents),
+    amount,
+    CategoryId: categoryId,
+    PaymentId,
+    UserId: userId,
+  };
+
+  const { err: historyErr } = await createHistory(data);
+  if (!historyErr) {
+    res.send({ ok: true, message: HISTORIES_POST_SUCCESS });
+  } else {
+    res.send({ ok: false, err: historyErr });
+  }
+});
 
 router.put('/', async (req, res) => {
-  const { date, categoryId, contents, payment, amount } = req.body;
+  const { date, categoryId, contents, payment, amount, userId } = req.body;
   const { id } = req.query;
   const formattedDate = formatDate(date);
-  const userId = 1;
 
-  try {
-    const PaymentId = await Payment.find({
-      where: {
-        UserId: userId,
-        method: payment
-      }
-    });
-
-    await History.update({
-      date: formattedDate,
-      contents: encodeHTML(contents),
-      amount,
-      CategoryId: categoryId,
-      PaymentId,
-      UserId: userId, 
-    }, {
-      where: {
-        id
-      }
-    })
-    res.send({ ok: true, message: HISTORIES_PUT_SUCCESS });
-  } catch(err) {
-    console.error(err);
+  const paymentWhereOptions = {
+    UserId: userId,
+    method: payment,
+  };
+  const { result: PaymentId, err } = await getPaymentId(paymentWhereOptions);
+  if (err) {
+    res.send({ ok: false, err });
+    return;
   }
-})
+
+  const newData = {
+    date: formattedDate,
+    contents: encodeHTML(contents),
+    amount,
+    CategoryId: categoryId,
+    PaymentId,
+    UserId: userId,
+  };
+  const whereOptions = {
+    where: {
+      id,
+    },
+  };
+  const { historyErr } = await updateHistory(newData, whereOptions);
+  if (!historyErr) {
+    res.send({ ok: true, message: HISTORIES_PUT_SUCCESS });
+  } else {
+    res.send({ ok: false, err: historyErr });
+  }
+});
 
 module.exports = router;
