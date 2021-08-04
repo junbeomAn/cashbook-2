01a08398e62d317280523328d572279d512de291
junbeoman <wnsqja8744@gmail.com>
2021-08-04 17:56:31 +0900

    feat/add cors and fix error message when server error occured

--- server/app.js
+++ server/app.js
@@ -3,15 +3,18 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
-const { sequelize } = require('./db/models');
+const cors = require('cors');
+const dotenv = require('dotenv');
 
+const { sequelize } = require('./db/models');
 const indexRouter = require('./routes/index');
-const usersRouter = require('./routes/users');
-const historyRouter = require('./routes/history');
-const paymentRouter = require('./routes/payment');
+
+dotenv.config();
 
 const app = express();
 sequelize.sync();
+
+app.use(cors());
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -20,9 +23,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/history', historyRouter);
-app.use('/payment', paymentRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -31,13 +31,10 @@ app.use(function (req, res, next) {
 
 // error handler
 app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.send('error');
+  res.send('Server Error Occured..');
 });
 
 app.listen(process.env.PORT || 5000, () => {
